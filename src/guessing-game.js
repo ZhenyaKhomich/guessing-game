d77b4c31a53a3b1b233deefc593a8d1a8a80dea5
Zhenya Khomich <zhenyatsoy21061992@gmail.com>
2024-08-26 15:12:03 +0400

    finish

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.currentGuess = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.guess;
     }
 
     guess() {
-
+        this.currentGuess = Math.round((this.min + this.max) / 2);
+        return this.currentGuess;
     }
 
     lower() {
-
+        this.max = this.currentGuess - 0,5;
     }
 
     greater() {
-
+        this.min = this.currentGuess + 0,5;
     }
 }
 
